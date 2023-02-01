3a437aad39d1892e0a9887737cc6b2a205792841
selimdoyranli <selim@radkod.com>
2023-02-01 14:52:52 +0300

    refactor: ux improvements

--- components/Menu/AspectRatioSelectMenu/AspectRatioSelectMenu.component.vue
+++ components/Menu/AspectRatioSelectMenu/AspectRatioSelectMenu.component.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
 .aspect-ratio-select-menu
+  // Standard List
   .aspect-ratio-select-menu__group.aspect-ratio-select-menu__group--standard
     .aspect-ratio-select-menu-item(
       v-for="ratio in aspectRatio.standard.list"
       :class="[getSelectedClass(ratio)]"
       @click="selectRatio(ratio)"
     )
+      span.aspect-ratio-select-menu-item__title {{ ratio.title }}
+
+  // Social List
+  .aspect-ratio-select-menu__group.aspect-ratio-select-menu__group--social
+    .aspect-ratio-select-menu-item(v-for="ratio in aspectRatio.social.list" :class="[getSelectedClass(ratio)]" @click="selectRatio(ratio)")
+      AppIcon.aspect-ratio-select-menu-item__icon(v-if="ratio.icon" :name="ratio.icon")
       span.aspect-ratio-select-menu-item__title {{ ratio.title }}
 </template>
 
@@ -75,7 +82,38 @@ export default defineComponent({
         ]
       },
       social: {
-        list: []
+        list: [
+          {
+            key: '1/1',
+            title: 'Instagram Post (1080x1080)',
+            icon: 'ri:instagram-line',
+            ratio: 1 / 1
+          },
+          {
+            key: '9/16',
+            title: 'Instagram Story (1080x1920)',
+            icon: 'ri:instagram-line',
+            ratio: 9 / 16
+          },
+          {
+            key: '16/9',
+            title: 'Youtube Thumbnail (1280x720)',
+            icon: 'ri:youtube-line',
+            ratio: 16 / 9
+          },
+          {
+            key: '851/315',
+            title: 'Facebook Cover (851x315)',
+            icon: 'ri:facebook-circle-line',
+            ratio: 851 / 315
+          },
+          {
+            key: '235/197',
+            title: 'Facebook Post (940x788)',
+            icon: 'ri:facebook-circle-line',
+            ratio: 235 / 197
+          }
+        ]
       }
     })
 
