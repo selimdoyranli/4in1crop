fed4cece073b7ae4d548c0145a68f12ddac36608
selimdoyranli <selim@radkod.com>
2023-02-03 19:26:49 +0300

    refactor: ui improvements

--- components/Dialog/AppCreditsDialog/AppCreditsDialog.component.vue
+++ components/Dialog/AppCreditsDialog/AppCreditsDialog.component.vue
@@ -8,6 +8,21 @@ vs-dialog.app-credits-dialog(v-model="dialog.isOpen" blur @close="handleClose")
     vs-button.mx-auto(transparent href="https://github.com/selimdoyranli/4in1crop" blank)
       AppIcon.me-1(name="charm:github" color="var(--color-text-02)")
       span.color-text-02 Github repository
+
+    // Buymeacoffee Button
+    script(
+      type="text/javascript"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+      data-name="bmc-button"
+      data-slug="selimdoyranli"
+      data-color="#FFDD00"
+      data-emoji
+      data-font="Inter"
+      data-text="Buy me a coffee"
+      data-outline-color="#000000"
+      data-font-color="#000000"
+      data-coffee-color="#ffffff"
+    )
 </template>
 
 <script>
